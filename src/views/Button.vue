ba43786ec8a575f1efec668d892046943b13b814
timelessover <shanpaodaxia@qq.com>
2019-03-30 22:22:21 +0800

    button

---
+++ src/views/Button.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id='button'>
+        <button>点我啊</button>    
+    </div>    
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
